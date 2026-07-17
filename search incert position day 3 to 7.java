d4ee8f8f97b6d58b83fe81cf2b42a4bfa1270747
yughabharathm-cmd <yughabharathm@gmail.com>
2026-07-17 11:08:38 +0530

    Create search incert position day 3 to 7.java

---
+++ search incert position day 3 to 7.java
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return left;
+    }
+};
