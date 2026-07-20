cd85296a402d955ec6a76886e5d2034c62caff31
yughabharathm-cmd <yughabharathm@gmail.com>
2026-07-20 13:21:45 +0530

    Create day 6-14 longest common prefix.java

---
+++ day 6-14 longest common prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0][i];
+            
+            for (int j = 1; j < strs.size(); j++) {
+                // If we reach the end of a string or characters don't match
+                if (i == strs[j].length() || strs[j][i] != c) {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+};
