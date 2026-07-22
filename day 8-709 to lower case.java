55338802595b2453a9f37fb960539c75fcfeb13e
yughabharathm-cmd <yughabharathm@gmail.com>
2026-07-22 13:41:01 +0530

    Create day 8-709 to lower case.java

---
+++ day 8-709 to lower case.java
@@ -0,0 +1,13 @@
+#include <string>
+
+class Solution {
+public:
+    std::string toLowerCase(std::string s) {
+        for (char& c : s) {
+            if (c >= 'A' && c <= 'Z') {
+                c += ('a' - 'A'); // Equivalent to c += 32
+            }
+        }
+        return s;
+    }
+};
