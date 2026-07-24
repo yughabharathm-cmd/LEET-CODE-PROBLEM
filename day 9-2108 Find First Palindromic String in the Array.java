644d6330763338e7311f8c20a2ef47d5f4330d8e
yughabharathm-cmd <yughabharathm@gmail.com>
2026-07-24 09:02:17 +0530

    Create day 9-2108 Find First Palindromic String in the Array.java

---
+++ day 9-2108 Find First Palindromic String in the Array.java
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool isPalindrome(const string& s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+public:
+    string firstPalindrome(vector<string>& words) {
+        for (const string& word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+};
